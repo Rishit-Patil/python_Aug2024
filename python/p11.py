f2bff18ef59dfa08ebd142dcb77be7500c1f36d9
Rishit-Patil <rishit6378@gmail.com>
2024-08-21 15:07:39 +0530

    Loop Programs update

--- python/p11.py
+++ python/p11.py
@@ -1,1 +1,13 @@
 #Program to Find Sum of Digits of a Number
+number=int(input("Enter a Number to Find the Sum of its Digits: "))
+k=0
+add=0
+old=number
+while(number>0):
+    k=number%10
+    add+=k
+    number=number/10
+print("The Sum of Digits of ",old," is :",int(add))
+    
+
+
