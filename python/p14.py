af1eb529b803126eb0519c2b2e740eb87a78ff16
Rishit-Patil <rishit6378@gmail.com>
2024-08-21 15:56:16 +0530

    Loop Programs update

--- python/p14.py
+++ python/p14.py
@@ -1,1 +1,10 @@
 #Program to find the Second Smallest Digit a Number
+number=int(input("Enter a Number to Find its Second Smallest Digit: "))
+string=[]
+old = number
+while number>0:
+    k=number%10
+    string.append(k)
+    number=number//10
+ascending_string = sorted(string)
+print("The Second Smallest Digit in",old,"is :", ascending_string[1])
