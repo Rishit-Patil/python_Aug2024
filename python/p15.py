fe0e0e21ad0b983752bcf0811a3785762d024c80
Rishit-Patil <rishit6378@gmail.com>
2024-08-21 16:15:37 +0530

    Loop Programs update

--- python/p15.py
+++ python/p15.py
@@ -1,1 +1,10 @@
 #Program to find the Sum of even(odd) digits in a number
+sample_number = int(input("Enter a number: "))
+old = sample_number
+even_sum = 0
+while sample_number>0:
+    temp = sample_number%10
+    if temp%2==0:
+        even_sum = even_sum + temp
+    sample_number=sample_number//10
+print("Sum of Even Digits in",old,"is:",int(even_sum))
