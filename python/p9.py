39dcc9a0fcd15f5f6c2f46f013a4c43717dfd295
Rishit-Patil <rishit6378@gmail.com>
2024-08-21 14:35:21 +0530

    Updation

--- python/p9.py
+++ python/p9.py
@@ -2,4 +2,4 @@
 sample_number = int(input("Enter a number: "))
 print(f"Table of {sample_number}:\n")
 for i in range(1,11):
-    print(f"{sample_number} x {i} = {sample_number*i}")
+    print("%02d * %02d = %03d"%(sample_number,i,sample_number*i))
