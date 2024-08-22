8cd607a054177e59c0dc6e8ce8367e16285aa466
Rishit-Patil <rishit6378@gmail.com>
2024-08-22 14:22:29 +0530

    Assignment Problems Updation

--- python/p17.py
+++ python/p17.py
@@ -1,1 +1,16 @@
 #Program to find Odd ( Even ) Placed digits in a Number 
+sample_number=int(input("Enter A Number: "))
+str_1=str(sample_number)
+print("The Odd Digits in the",sample_number,"are:")
+for i in range (0,len(str_1),2):
+    print(str_1[i])
+
+
+
+
+
+
+
+
+
+    
--- python/p18.py
+++ python/p18.py
@@ -1,1 +1,14 @@
 #Program to Find Odd Placed Even Digits in a number 
+sample_number=int(input("Enter A Number: "))
+str_1=str(sample_number)
+print("The Odd Placed Even Digits in the",sample_number,"are:")
+for i in range (0,len(str_1),2):
+    if (int(str_1[i])%2==0):
+        print(str_1[i])
+
+
+
+
+
+
+
